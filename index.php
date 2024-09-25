e3450b919e62aef92c3c4e749aea28a6db672cf8
Xavi Guti <114662252+xaviguti02@users.noreply.github.com>
2024-09-25 19:14:02 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE hmtl>
+<html lang="es">
+    <head>
+      <meta charset="UTF-8">
+      <title>Demo ASIX</title>
+    </head>
+    <body>
+        <h1>
+            <?php
+
+                echo "Hola mundo!";
+
+            ?>
+        </h1>
+    </body>
+</html>
